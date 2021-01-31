36a74b56ad6ae91c45802bbf21bb10ae9e7a37b2
Mohammed Tirichine <mohtiri718@gmail.com>
2021-01-31 11:13:18 +0100

    user friendly

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ You know the rules of course :)
 
 to control just use arrows
 
+and of course you don't wanna hit ANYTHING on your way
+
 try to stay longer
---
+++ welcome.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!\nClick Enter To Begin" << endl;
+    cin.get();
+    return 0;
+}
